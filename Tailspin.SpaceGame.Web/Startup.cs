d59c09be2548de2e97704d0d102dd50c9f9188ae
JohnSysConsulting <jonh.bart14@gmail.com>
2021-04-20 17:28:31 +0200

    my first commit wesh

--- Tailspin.SpaceGame.Web/Startup.cs
+++ Tailspin.SpaceGame.Web/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Hosting;
 using TailSpin.SpaceGame.Web.Models;
 using Microsoft.AspNetCore.Http;
 
-
+// My first change
 namespace TailSpin.SpaceGame.Web
 {
     public class Startup
